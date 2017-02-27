6fcf6f35ec2b759a277629498fde89d2cf5cd946
PrismTower <PrismTower@mail.com>
2017-02-27 20:47:13 +0800

    FFT performance optimized

--- BigInteger_FFT.cpp
+++ BigInteger_FFT.cpp
@@ -19,8 +19,8 @@ namespace UPmath
 
 #define FFT_WORD_SIZE (FFT_WORD_BITLEN / 8)
 		/*const union {
-		uint32 uint_value;
-		char char_value[4];
+			uint32 uint_value;
+			char char_value[4];
 		} _EndianCheckUnion = { 0x11223344 };
 		constexpr bool _IS_LITTLE_ENDIAN() { return _EndianCheckUnion.char_value[0] == 0x44; }*/
 
@@ -62,38 +62,41 @@ namespace UPmath
 
 		inline uint32 getIndexOfPower(uint32 omegaIndex, uint32 pow) { return (omegaIndex * pow) % FFT_MAX_N; }
 
-		//`InputType` should be either `Coefficients` (isReverse = false)  or  `ValueRepresentation` (isReverse = true)
+		//`InputType` is either `Coefficients` (isReverse = false)  or  `ValueRepresentation` (isReverse = true, reverse transform)
 		template <bool isReverse, typename InputType>
-		void FFT(Complex* dst, Complex* buffer, const InputType& src, uint32 omegaIndex)
+		void FFT(Complex* dst, InputType& src, uint32 omegaIndex)
 		{//Warning: src.n cannot be smaller than 4
 			if (src.n <= 4) {
 				Complex x = src.getComplexNumber(0);
 				Complex y = src.getComplexNumber(src.interval * 2);
-				buffer[0] = Complex::add(x, y);
-				buffer[1] = Complex::subtract(x, y);
+				Complex buffer0 = Complex::add(x, y);
+				Complex buffer1 = Complex::subtract(x, y);
 				x = src.getComplexNumber(src.interval);
 				y = src.getComplexNumber(src.interval * 3);
-				buffer[2] = Complex::add(x, y);
-				buffer[3] = Complex::subtract(x, y);
-				Complex wjAo = buffer[2];
-				dst[0] = Complex::add(buffer[0], wjAo);
-				dst[2] = Complex::subtract(buffer[0], wjAo);
-				wjAo = !isReverse ? Complex(-buffer[3].im, buffer[3].re) : Complex(buffer[3].im, -buffer[3].re);
-				dst[1] = Complex::add(buffer[1], wjAo);
-				dst[3] = Complex::subtract(buffer[1], wjAo);
+				Complex buffer2 = Complex::add(x, y);
+				Complex buffer3 = Complex::subtract(x, y);
+				Complex wjAo = buffer2;
+				dst[0] = Complex::add(buffer0, wjAo);
+				dst[2] = Complex::subtract(buffer0, wjAo);
+				wjAo = !isReverse ? Complex(-buffer3.im, buffer3.re) : Complex(buffer3.im, -buffer3.re);
+				dst[1] = Complex::add(buffer1, wjAo);
+				dst[3] = Complex::subtract(buffer1, wjAo);
 				return;
 			}
-			InputType A(src);
-			A.interval <<= 1;
-			A.n >>= 1;
-			FFT<isReverse>(buffer, dst, A, getIndexOfPower(omegaIndex, 2));//A even
-			A.begin += src.interval;
-			FFT<isReverse>(buffer + A.n, dst, A, getIndexOfPower(omegaIndex, 2));//A odd
-			for (uint32 j = 0; j < A.n; ++j) {
-				Complex wjAo = Complex::multiply(kRootsOfUnity[getIndexOfPower(omegaIndex, j)], (buffer + A.n)[j]);
-				dst[j] = Complex::add(buffer[j], wjAo);
-				(dst + A.n)[j] = Complex::subtract(buffer[j], wjAo);
+			auto srcBeginBackup = src.begin + src.interval;
+			src.interval <<= 1;
+			src.n >>= 1;
+			FFT<isReverse>(dst, src, getIndexOfPower(omegaIndex, 2));//A even
+			std::swap(src.begin, srcBeginBackup);
+			FFT<isReverse>(dst + src.n, src, getIndexOfPower(omegaIndex, 2));//A odd
+			std::swap(src.begin, srcBeginBackup);
+			for (uint32 j = 0; j < src.n; ++j) {
+				Complex wjAo = Complex::multiply(kRootsOfUnity[getIndexOfPower(omegaIndex, j)], (dst + src.n)[j]);
+				(dst + src.n)[j] = Complex::subtract(dst[j], wjAo);
+				dst[j] = Complex::add(dst[j], wjAo);
 			}
+			src.interval >>= 1;
+			src.n <<= 1;
 		}
 
 	}//namespace BigInteger_FFT_Precedures
@@ -110,20 +113,20 @@ namespace UPmath
 			_isRootsOfUnityInitialized = true;
 		}
 		bool newMemAlloced = (nullptr == buffer);
-		if (newMemAlloced) buffer = operator new(n * 3 * sizeof(BigInteger_FFT_Precedures::Complex));
+		if (newMemAlloced) buffer = operator new(n * 2 * sizeof(BigInteger_FFT_Precedures::Complex));
 		BigInteger_FFT_Precedures::Complex* _buffer = reinterpret_cast<BigInteger_FFT_Precedures::Complex*>(buffer);
 		//multi-threading benefits little here
 		BigInteger_FFT_Precedures::Coefficients A(lhs, n);
-		BigInteger_FFT_Precedures::Complex* lhsValues = _buffer + n;
-		BigInteger_FFT_Precedures::FFT<false>(lhsValues, _buffer, A, kOmegaIndex);
+		BigInteger_FFT_Precedures::Complex* lhsValues = _buffer;
+		BigInteger_FFT_Precedures::FFT<false>(lhsValues, A, kOmegaIndex);
 		BigInteger_FFT_Precedures::Coefficients B(rhs, n);
 		BigInteger_FFT_Precedures::Complex* rhsValues = lhsValues + n;
-		BigInteger_FFT_Precedures::FFT<false>(rhsValues, _buffer, B, kOmegaIndex);
-		for (uint32 j = 0; j < n; ++j)
-			_buffer[j] = BigInteger_FFT_Precedures::Complex::multiply(*lhsValues++, *rhsValues++);
-		BigInteger_FFT_Precedures::ValueRepresentation C(_buffer, n);
-		BigInteger_FFT_Precedures::Complex* products = _buffer + n;
-		BigInteger_FFT_Precedures::FFT<true>(products, products + n, C, FFT_MAX_N - kOmegaIndex);
+		BigInteger_FFT_Precedures::FFT<false>(rhsValues, B, kOmegaIndex);
+		for (uint32 j = 0; j < n; ++j, ++rhsValues)
+			*rhsValues = BigInteger_FFT_Precedures::Complex::multiply(*lhsValues++, *rhsValues);
+		BigInteger_FFT_Precedures::ValueRepresentation C(rhsValues - n, n);//C[i] = A[i] * B[i]
+		BigInteger_FFT_Precedures::Complex* products = _buffer;
+		BigInteger_FFT_Precedures::FFT<true>(products, C, FFT_MAX_N - kOmegaIndex);
 
 		uint32 newCapa = n / (BITS_OF_DWORD / FFT_WORD_BITLEN);
 		if (newCapa > dst.capacity) {
@@ -132,18 +135,19 @@ namespace UPmath
 		}
 		unsigned long long t = 0;
 		static_assert(sizeof(unsigned long long) > sizeof(uint32), "");
-		for (uint32 pieceIndex = 0; pieceIndex < newCapa; ++pieceIndex) {
+		const double reciprocal_of_n = 1.0 / n;
+		for (uint32 pieceIndex = 0; pieceIndex < newCapa; ++pieceIndex)	{
 			//double er = abs(round((products)->re / n) - (products)->re / n); if (er > __dbgFFTMaxError) __dbgFFTMaxError = er;
-			t += (unsigned long long)((products++)->re / n + 0.5);
+			t += (unsigned long long)((products++)->re * reciprocal_of_n + 0.5);
 			dst._valPtr[pieceIndex] = t & ((1 << FFT_WORD_BITLEN) - 1);
 			t >>= FFT_WORD_BITLEN;
 			//er = abs(round((products)->re / n) - (products)->re / n); if (er > __dbgFFTMaxError) __dbgFFTMaxError = er;
-			t += (unsigned long long)((products++)->re / n + 0.5);
-			dst._valPtr[pieceIndex] |= uint32(t) << FFT_WORD_BITLEN;
+			t += (unsigned long long)((products++)->re * reciprocal_of_n + 0.5);
+			dst._valPtr[pieceIndex] |= (uint32(t) << FFT_WORD_BITLEN);
 			t >>= FFT_WORD_BITLEN;
 		}
 		dst._setSize(newCapa);
-
+		
 		if (newMemAlloced) operator delete(buffer);
 	}
 
